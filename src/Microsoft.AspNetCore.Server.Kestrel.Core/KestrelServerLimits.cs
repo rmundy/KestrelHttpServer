af00f9c85909debde1b14a95289c709bd30c97c4
Cesar Blum Silveira <cesars@microsoft.com>
2017-06-01 13:55:58 -0700

    XML docs.

--- src/Microsoft.AspNetCore.Server.Kestrel.Core/KestrelServerLimits.cs
+++ src/Microsoft.AspNetCore.Server.Kestrel.Core/KestrelServerLimits.cs
@@ -208,6 +208,9 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core
             }
         }
 
+        /// <summary>
+        /// Gets or sets the default timeout for receiving the request body. Defaults to 2 minutes.
+        /// </summary>
         public RequestBodyTimeout DefaultRequestBodyTimeout { get; } = new RequestBodyTimeout();
     }
 }
